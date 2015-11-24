572d08edc85ec16dca723f9ae8a79daca79338f9
Snarna <xyq163446@gmail.com>
2015-11-24 11:22:22 -0500

    delete error file
    connectServer.php

--- myindex.php
+++ myindex.php
@@ -1,3 +1,18 @@
 <?php
-echo "Hello World!";
+$servername = "localhost:3306";
+$username = "snarna";
+$password = "limingjie";
+$database = "apksdb";
+
+$con = new mysqli($servername, $username, $password, $database);
+
+if($con->connect_error){
+  die("Connection Failed!" . $con->connect_error);
+}
+
+echo "Connected!";
+
+$con->close();
+echo "Connection Closed!";
+
 ?>
